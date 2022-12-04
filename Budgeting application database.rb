cf07fa64415540379da993e655bc07dbf1ab157f
britton477 <axbritton@gmail.com>
2022-12-04 15:58:26 +0000

    Added ability to add, change and display expenses also saves budget total. Need to return total after deduction of expenses, ability to remove expenses next. I AM EVOLVING

--- Budgeting application database.rb
+++ Budgeting application database.rb
@@ -3,20 +3,23 @@ require './sqlite_budget'
 class BudgetingApp
 
   def initialize
-    @total = 0
     @expenses_hash = Hash.new
     print "Enter username: "
     @user_name = gets.chomp
-    get_total
+    incomeTotal
     menu
-    return 
   end 
 
-  def get_total
-  	print "Current budget: £"
-  	@total = gets.chomp.to_i
+  def incomeTotal
+    user_budget = executeSelectQuery("expenses.db", "select * from budget where name = '#{@user_name}'")
+    if user_budget == []
+      print "Current income: £"
+      income = gets.chomp.to_f
+      executeQuery("expenses.db", "INSERT INTO budget ('name','budget_name','budget_value') 
+        VALUES ('#{@user_name}','Income','#{income}')")
+    end
   end
-  
+
   def menu
     while true
       puts "\n"
@@ -27,23 +30,27 @@ class BudgetingApp
       puts "1) Add expenses"
       puts "2) Update expenses"
       puts "3) Display current expenses"
-      puts "4) Display remaining total"
-      puts "5) Exit"
+      puts "4) Display current budgets"
+      puts "5) Display remaining budget"
+      puts "6) Exit"
       puts "__________________________________________"
       print ": "
 
       choice = gets.chomp
       case choice
         when "1"
-          add_expenses
+          addExpenses
         when "2"
-          update_expense
+          updateExpense
         when "3"
-          my_array = executeSelectQuery("expenses.db", "select * from expense where name = '#{@user_name}'")
-          my_array.each {|expense| puts expense}
+          show_expense = executeSelectQuery("expenses.db", "select * from expense where name = '#{@user_name}'")
+          show_expense.each {|expense| puts expense}
         when "4"
-          puts "Current total: #{@total}\n"
+          show_budget = executeSelectQuery("expenses.db", "select * from budget where name = '#{@user_name}'")
+          show_budget.each {|budget| puts budget}
         when "5"
+          remainingBudget
+        when "6"
           break
       end
     end
@@ -59,39 +66,33 @@ class BudgetingApp
     end
   end
 
-  def add_expenses
+  def addExpenses
 
     flag = true
     while flag
       print "Name of expense: "
       expense = gets.chomp.to_sym
       print "Value of expense: £"
-      value = gets.chomp.to_i
-      @total -= value
-      @expenses_hash[user_name][expense] = value
-
+      value = gets.chomp.to_f
+      executeQuery("expenses.db", "INSERT INTO expense ('name','ex_name','ex_value') 
+        VALUES ('#{@user_name}','#{expense}','#{value}')")
+      #@total -= value
       flag = false if repeat
     end
     puts "New total: £#{@total}"
   end
 
-  def update_expense
+  def updateExpense
 
     flag = true
     while flag
       print "Expense to update: "
-      search = gets.chomp.to_sym
-      if @expenses_hash[search] != nil
+      search = gets.chomp
+      expense_search = executeSelectQuery("expenses.db", "SELECT * from expense where ex_name = '#{search}'")
+      if expense_search != []
         print "New value: £"
-        new_value = gets.chomp.to_i
-        old_value = @expenses_hash[search]
-        diff = (old_value - new_value).abs
-        if new_value > old_value
-          @total -= diff
-        else
-          @total += diff
-        end
-        @expenses_hash[search] = new_value
+        new_value = gets.chomp.to_f
+        executeQuery("expenses.db", "UPDATE expense SET ex_value = '#{new_value}' where ex_name = '#{search}'")
         puts "Expense updated"
         puts "New total: £#{@total}"
       else 
@@ -99,6 +100,16 @@ class BudgetingApp
       end
       flag = false if repeat
     end
+  end
+
+  #def remainingBudget
+
+    #all_expense = executeSelectQuery("expenses.db", "SELECT ex_value from expense")
+    #total = executeSelectQuery("expenses.db", "SELECT budget_value from budget where name = '#{@user_name}' and budget_name = 'Income'")
+    #puts all_expense
+    #puts total
+    #final_total = all_expense.each {|expense| total -= expense}
+
   end
 end
 
